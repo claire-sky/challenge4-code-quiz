713d01d88ef67b0a62fbd7cfe5be63b4141f479a
Claire Sky <eclconnolly@gmail.com>
2021-10-31 13:09:03 -0700

    timer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
+var timer = 0;
+var timerEl = document.getElementById('timeLeft');
+var score = 0;
 var questions = [
-    {
+        {
         question: "What does DOM stand for?",
         answers: {
             1: "Display Order Method",
@@ -50,3 +53,68 @@ var questions = [
         correctAnswer: '3'
     },
 ]
+
+// quiz start screen with button
+function startQuiz() {
+        "Coding Quiz Challenge",
+        "See if you can answer all 5 questions in 30 seconds. For fun, incorrect answers result in a 5 second penalty! ;)",
+        "Start Quiz"
+    countdown();
+};
+
+// function to build quiz
+function buildQuiz() {
+    var output = [];
+
+    // for each question
+    for(var i = 0; i < questions.length; i++) {
+        var answers = [];
+
+        // for each answer
+        for(number in questions[i].answers) {
+            answers.push(
+                `<button type="button">${number}: ${questions[i].answers[number]}</button>`
+            );
+        }
+
+        output.push(
+            `<div class="question"> ${questions[i].question} </div>
+            <div class="answers">${answers.join('')} + </div>`
+        );
+    }
+
+    quizContainer.innerHTML = output.join('');
+};
+
+// function to show answer
+function showAnswer() {
+
+    // right answer adds 20pts to score
+
+    // wrong answer removes 5000 miliseconds from timer
+
+};
+
+// function to end game
+function endGame() {
+
+};
+
+// timer
+function countdown() {
+    var timer = 30;
+    var timeInterval = setInterval(function() {
+    if (timer > 1) {
+      timerEl.textContent = timer;
+      timer = timer - 1;
+    }
+    else {
+      timerEl.textContent = 0;
+      clearInterval(timeInterval);
+    //   endGame();
+    };
+  }, 1000);
+};
+
+
+countdown();

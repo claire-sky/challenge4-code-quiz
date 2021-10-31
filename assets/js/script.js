8e1d28110eb50180cb299de2a437f6144fb26b4e
Claire Sky <eclconnolly@gmail.com>
2021-10-31 10:10:57 -0700

    Html setup

---
+++ assets/js/script.js
@@ -0,0 +1,52 @@
+var questions = [
+    {
+        question: "What does DOM stand for?",
+        answers: {
+            1: "Display Order Method",
+            2: "Document Order Model",
+            3: "Display Object Method",
+            4: "Document Object Model"
+        },
+        correctAnswer: '4'
+    },
+    {
+        question: "Which has a condition and will only stop running the code once the condition returns",
+        answers: {
+            1: "loops",
+            2: "conditionals",
+            3: "switch",
+            4: "break"
+        },
+        correctAnswer: '1'
+    },
+    {
+        question: "Which is not a data type?",
+        answers: {
+            1: "String",
+            2: "Method",
+            3: "Boolean",
+            4: "null"
+        },
+        correctAnswer: '2'
+    },
+    {
+        question: "Which displays a dialog box with a specified message, along with an OK and a Cancel button?",
+        answers: {
+            1: "alert",
+            2: "prompt",
+            3: "confirm",
+            4: "deny"
+        },
+        correctAnswer: '3'
+    },
+    {
+        question: "Which is not an operator?",
+        answers: {
+            1: "&",
+            2: "|",
+            3: "@",
+            4: "="
+        },
+        correctAnswer: '3'
+    },
+]
